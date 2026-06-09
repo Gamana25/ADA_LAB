f10570d5d81b6585eb02fcd002a7183f513031eb
Gamana25 <gamanasathvika.cs24@bmsce.ac.in>
2026-06-09 09:40:03 +0530

    Update johnsonsTrotter.c

--- LAB8/johnsonsTrotter.c
+++ LAB8/johnsonsTrotter.c
@@ -2,35 +2,31 @@
 
 int a[10], dir[10];
 
-void printPermutation(int n)
-{
+void printPermutation(int n) {
     int i;
 
-    for(i = 0; i < n; i++)
+    for(i = 0; i < n; i++) {
         printf("%d ", a[i]);
+    }
 
     printf("\n");
 }
 
-int mobile(int n)
-{
-    int i, mobile_prev = 0, mobile_index = -1;
+int mobile(int n) {
+    int i;
+    int mobile_prev = 0;
+    int mobile_index = -1;
 
-    for(i = 0; i < n; i++)
-    {
-        if(dir[a[i]-1] == -1 && i != 0)
-        {
-            if(a[i] > a[i-1] && a[i] > mobile_prev)
-            {
+    for(i = 0; i < n; i++) {
+        if(dir[a[i] - 1] == -1 && i != 0) {
+            if(a[i] > a[i - 1] && a[i] > mobile_prev) {
                 mobile_prev = a[i];
                 mobile_index = i;
             }
         }
 
-        if(dir[a[i]-1] == 1 && i != n-1)
-        {
-            if(a[i] > a[i+1] && a[i] > mobile_prev)
-            {
+        if(dir[a[i] - 1] == 1 && i != n - 1) {
+            if(a[i] > a[i + 1] && a[i] > mobile_prev) {
                 mobile_prev = a[i];
                 mobile_index = i;
             }
@@ -40,50 +36,50 @@ int mobile(int n)
     return mobile_index;
 }
 
-void johnsonTrotter(int n)
-{
+void johnsonTrotter(int n) {
     int i;
+    int m;
+    int swapIndex;
+    int temp;
 
-    for(i = 0; i < n; i++)
-    {
+    for(i = 0; i < n; i++) {
         a[i] = i + 1;
         dir[i] = -1;
     }
 
     printPermutation(n);
 
-    while(1)
-    {
-        int m = mobile(n);
+    while(1) {
+        m = mobile(n);
 
-        if(m == -1)
+        if(m == -1) {
             break;
+        }
 
-        int swapIndex;
-
-        if(dir[a[m]-1] == -1)
+        if(dir[a[m] - 1] == -1) {
             swapIndex = m - 1;
-        else
+        }
+        else {
             swapIndex = m + 1;
+        }
 
-        int temp = a[m];
+        temp = a[m];
         a[m] = a[swapIndex];
         a[swapIndex] = temp;
 
         m = swapIndex;
 
-        for(i = 0; i < n; i++)
-        {
-            if(a[i] > a[m])
-                dir[a[i]-1] *= -1;
+        for(i = 0; i < n; i++) {
+            if(a[i] > a[m]) {
+                dir[a[i] - 1] *= -1;
+            }
         }
 
         printPermutation(n);
     }
 }
 
-int main()
-{
+int main() {
     int n;
 
     printf("Enter value of n: ");
